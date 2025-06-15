c04d7bdef512189f16c248a894dd0af2f2a318ba
Knut Onsøyen <kgo@ingenia.no>
2025-06-15 21:04:41 +0200

    Oppdatert

--- src/pages/streetview.jsx
+++ src/pages/streetview.jsx
@@ -1,13 +1,36 @@
 import "../css/streetview.css"
 
 function Streetview() {
-    return <div className="About-empty">
-        <h2>Kontakt</h2>
-        <p>
-        Knut Onsøyen<br />
-        streetview incomming!!
-        </p>
-    </div>
+    return (
+        <>
+        <div className="streetView">
+            <h2>360 View</h2>
+            
+            <p>
+            Naviger deg rundt i bygg enkelt<br />
+            </p>
+
+            <div className="bilde-rad">
+                <a href="https://25028.vercel.app/">
+                    <img src = "/src/images/360.png" alt="bilde 1" />
+                    <p>UiO - BL18</p>
+                </a>
+                <a href="https://marzipano-psi.vercel.app/">
+                    <img src = "/src/images/Tannlegehøyskolen_Garderober.png" alt="bilde 1" />
+                    <p>Tannlegehøyskolen Garderober</p>
+                </a>
+                <a href="https://25012-tilfluktsrom.vercel.app/">
+                    <img src = "/src/images/Tilfluktsrom.png" alt="bilde 1" />
+                    <p>Tannlegehøyskolen Tilfluktsrom</p>
+                </a>
+                <a href="https://ansatte-toaletter.vercel.app/">
+                    <img src = "/src/images/Toaletter.png" alt="bilde 1" />
+                    <p>Tannlegehøyskolen Ansatte-toaletter</p>
+                </a>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default Streetview
