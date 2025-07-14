2bcf40432b8c6d397a6103fe38eaedc7b013adc5
Knut Onsøyen <kgo@ingenia.no>
2025-07-14 15:14:16 +0200

    update

--- src/pages/work_pages/delT.jsx
+++ src/pages/work_pages/delT.jsx
@@ -3,8 +3,8 @@ import "../../css/delT.css"
 function delT() {
     return <div className="delT">
                 <h2>Sett inn verdier og trykk på "=", finn endring i tempratur</h2>
-                <div className="delT-board">
-                    <iframe src="https://kalkulator-eta-black.vercel.app/" height="800" width="1000"></iframe>
+                <div className="container">
+                    <iframe class="responsive-iframe" src="https://kalkulator-eta-black.vercel.app/"></iframe>
                 </div>
             </div>
 }
