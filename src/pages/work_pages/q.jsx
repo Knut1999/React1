2ad46682a936773914c0a4b6899e989b72e2a5e0
Knut Onsøyen <kgo@ingenia.no>
2025-07-26 12:38:02 +0200

    new game

--- src/pages/work_pages/q.jsx
+++ src/pages/work_pages/q.jsx
@@ -2,7 +2,7 @@ import "../../css/delT.css"
 
 function delT() {
     return <div className="delT">
-                <h2>Sett inn verdier og trykk på "=", finn effekt som må¨til for å varme luften</h2>
+                <h2>Sett inn verdier og trykk på "=", finn effekt som må til for å varme luften</h2>
                 <div className="container">
                     <iframe class="responsive-iframe" src="https://kalkulator-mcpdel-t-for-q.vercel.app/"></iframe>
                 </div>
