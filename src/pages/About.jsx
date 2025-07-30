38fd51cc7914cab444d44e67b046deb064f7f5da
Knut Onsøyen <din@email.no>
2025-07-30 14:25:04 +0200

    Oppdatering

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ function About() {
         E-post: knut.onso@gmail.com <br />
         Nummer: +47 91 81 86 16 <br />
         <br />
-        Dette er et prosjekt laget for gøy. Det er ikke brukt AI for å lage noe på nettsiden
+        Dette er et prosjekt laget for gøy. Det er brukt minimalt med AI for å lage noe på nettsiden
         </p>
     </div>
 }
